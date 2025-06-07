e5af070709d9c4e185fdf45fbf2087ed6911f8e0
Yair Quintana <yairquintanamx@gmail.com>
2025-06-07 04:35:56 -0600

    feat(Models): Create blogpost model and update the schema

---
+++ app/models/blogpost.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from core.db.base import Base
+from datetime import datetime, timezone
+
+from .association_tables import blogpost_tags
+
+
+class Series(Base):
+    __tablename__ = "series"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, unique=True, index=True, nullable=False)
+
+    blogposts = relationship(
+        "Blogpost", back_populates="series", cascade="all, delete-orphan"
+    )
+
+
+class Blogpost(Base):
+    __tablename__ = "blogposts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, unique=True, index=True, nullable=False)
+    slug = Column(String, unique=True, index=True, nullable=False)
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    banner = Column(String, nullable=True)
+    content = Column(String, nullable=False)
+    preview = Column(String, nullable=True)
+    project_id = Column(Integer, ForeignKey("projects.id"), nullable=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    series_id = Column(Integer, ForeignKey("series.id"), nullable=True)
+    part_number = Column(Integer, nullable=True)
+    is_active = Column(Boolean, default=True)
+
+    author = relationship("User", back_populates="blogposts")
+    tags = relationship("Tag", secondary=blogpost_tags, back_populates="blogposts")
+    project = relationship("Project", back_populates="blogpost", uselist=False)
+    series = relationship("Series", back_populates="blogposts")
+
+    def __repr__(self):
+        return (
+            f"<Blogpost(id={self.id}, title={self.title}, author_id={self.author_id}, "
+            f"slug={self.slug}, created_at={self.created_at})>"
+        )
--- app/schemas/blogpost.py
+++ app/schemas/blogpost.py
@@ -2,17 +2,20 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+from .tag import TagBase
+
 
 class BlogpostBase(BaseModel):
     id: Optional[int]
     title: str
     slug: str
-    tags: list[str]
+    tags: list[TagBase] = []
     banner: str
     content: str
     preview: str
     author_id: int
+    project_id: Optional[int]
     created_at: datetime
-    previous_id: Optional[int]
-    next_id: Optional[int]
+    series_id: Optional[int]
+    part_number: Optional[int]
     is_active: bool

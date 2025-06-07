d95bb40061520586cc5cfa78e9b641e8799af725
Yair Quintana <yairquintanamx@gmail.com>
2025-06-07 04:41:48 -0600

    feat(DB): Create declarative base and session generator

---
+++ core/db/session.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from typing import Generator
+
+from core.config import settings
+
+
+DB_URL = settings.DB_URL
+engine = create_engine(DB_URL)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal
+        yield db
+    finally:
+        db.close()
